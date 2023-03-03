9a02023e69ccdf6fa8c5d698c780a20673d38f04
Tu Ngoc Van <tungocvan@gmail.com>
2023-03-03 20:32:43 +0700

    update branch hp

--- modules/Users/routes/routes.php
+++ modules/Users/routes/routes.php
@@ -2,4 +2,8 @@
  use Illuminate\Support\Facades\Route;
  use Modules\Users\src\Http\Controllers\UserController;
  
+ Route::get('/', function () {
+    return view('welcome');
+});
+
  Route::get('/user', [UserController::class,'index']);
